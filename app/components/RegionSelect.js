b5c5e6f038357d0cd99942c0bceacca09d66f417
= <=>
2021-10-16 15:56:47 -0400

    added class to options

--- app/components/RegionSelect.js
+++ app/components/RegionSelect.js
@@ -4,18 +4,17 @@ export function RegionSelect(props) {
     $select.innerHTML = `
         <select class="select">
             <option value="" selected disabled>Filter by region</option>
-            <option value="africa">Africa</option>
-            <option value="americas">Americas</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
+            <option class="option" value="africa">Africa</option>
+            <option class="option" value="americas">Americas</option>
+            <option class="option" value="asia">Asia</option>
+            <option class="option" value="europe">Europe</option>
+            <option class="option" value="oceania">Oceania</option>
         <select>
         <i class="uil uil-angle-down"></i>
     
     `
     document.addEventListener("click", e => {
-        if (e.target.matches("option")) {
-            alert("Test")
+        if (e.target.matches(".option")) {
             localStorage.setItem("region", e.target.value);
             location.hash = `#/region/${localStorage.getItem("region")}`;
         }
